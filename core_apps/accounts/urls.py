dcf7d647431e5e33a06e1aefd1f0263ee8772ac3
Ashmit <ashmit[pandey04@gmail.com>
2026-01-05 12:58:24 +0530

    tellers can deposit money into the bank account

--- core_apps/accounts/urls.py
+++ core_apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
-    AccountVerificationView
+    AccountVerificationView,
+    DepositView,
 )
 
 urlpatterns = [
@@ -8,4 +9,6 @@ urlpatterns = [
         "verify/<uuid:pk>/",
         AccountVerificationView.as_view(),
         name="account_verification",
-    ),]
+    ),
+     path("deposit/", DepositView.as_view(), name="account_deposit"),
+     ]
